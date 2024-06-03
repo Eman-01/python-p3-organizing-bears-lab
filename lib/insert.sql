5ea1647c3092166fdd31a5add943929eb55cb6ce
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2024-06-03 20:42:32 +0300

    lab completed

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+bears (id, name, age, sex, color, temperament, alive) 
+VALUES 
+(1,'Mr. Chocolate', 5, 'M', 'Brown', 'Chill', True),
+(2,'Rowdy', 7, 'F', 'Purple', 'Rowdy', True),
+(3,'Tabitha', 1, 'F', 'Yellow', 'Evil', False),
+(4,'Sergeant Brown', 8, 'M', 'Brown', 'Relaxed', True),
+(5,'Melissa', 13, 'F', 'Pink', 'Hungry', True),
+(6,'Grinch', 20, 'M', 'Green', 'Sleepy', True),
+(7,'Wendy', 40, 'F', 'Black', 'Wild', True),
+(8,NULL, 60, 'M', 'Green', 'Wild', True);
